f907b4fd6c75aa5b0e9b743442333bf99176fcc1
jiangzhenkui <552039447@qq.com>
2019-10-12 18:44:48 +0800

    手机号校验类修改

--- src/PhoneValidator.php
+++ src/PhoneValidator.php
@@ -12,7 +12,7 @@ class PhoneValidator implements ValidatorInterface
 
     public function check($value, $rule)
     {
-        $pattern = '/1(3[0-9]|4[5,7]|5[0-9]|7[01687]|8[0-9])\d{8}/';
+        $pattern = '/^1[23456789][0-9]{9}$/';
 
         return boolval(preg_match($pattern, $value));
     }
